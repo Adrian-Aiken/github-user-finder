4ff23712c83fb2e6def9b0e5b92aca176b8cffca
Adrian Aiken <goatmancer@gmail.com>
2018-04-05 17:51:55 -0700

    Basic generation and stubbed out content

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name githubUserFinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the githubUserFinderApp
+ */
+angular.module('githubUserFinderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
